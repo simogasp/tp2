29d1667aab480415e2770fb8eca4e4dc47965ec2
Simone Gasparini <simone.gasparini@gmail.com>
2020-04-29 16:35:55 +0200

    test sol

--- robot.c
+++ robot.c
@@ -28,33 +28,63 @@ int Angle2 = 0;
 int RobotAngleX = 0;
 int RobotAngleY = 0;
 
-
+// how open are the pincers [0,1]
+#define START 0.125
+#define END 0.5
+float open = 1.0f;
 
 //*************************************************************************
 // Function that draws a reference system
 //*************************************************************************
 void DrawReferenceSystem()
 {
-	//**********************************
-	// set the line width to 3.0
-	//**********************************
-
-	//**********************************
-	// Draw three lines along the x, y, z axis to represent the reference system
-	// Use red for the x-axis, green for the y-axis and blue for the z-axis
-	//**********************************
-
-
-
-
-	//**********************************
-	// reset the drawing color to white
-	//**********************************
-
-	//**********************************
-	// reset the line width to 1.0
-	//**********************************
-
+    //**********************************
+    // set the line width to 3.0
+    //**********************************
+    glLineWidth (3.0);
+    //**********************************
+    // Start to draw the lines as a set of vertices
+    //**********************************
+    glBegin (GL_LINES);
+    //**********************************
+    // Set the color red for the x-axis
+    //**********************************
+    glColor3f ( 1, 0, 0);
+    //**********************************
+    // Set TWO ending points for the line representing the x-axis
+    //**********************************
+    glVertex3f (0, 0, 0);
+    glVertex3f (1, 0, 0);
+    //**********************************
+    // Set the color green for the y-axis
+    //**********************************
+    glColor3f (0, 1, 0);
+    //**********************************
+    // Set TWO ending points for the line representing the y-axis
+    //**********************************
+    glVertex3f (0, 0, 0);
+    glVertex3f (0, 1, 0);
+    //**********************************
+    // Set the color blue for the z-axis
+    //**********************************
+    glColor3f (0, 0, 1);
+    //**********************************
+    // Set TWO ending points for the line representing the z-axis
+    //**********************************
+    glVertex3f (0, 0, 0);
+    glVertex3f (0, 0, 1);
+    //**********************************
+    // End the drawing
+    //**********************************
+    glEnd();
+    //**********************************
+    // reset the drawing color to white
+    //**********************************
+    glColor3f (1,1, 1);
+    //**********************************
+    // reset the line width to 1.0
+    //**********************************
+    glLineWidth (1.0);
 }
 
 
@@ -63,51 +93,77 @@ void DrawReferenceSystem()
 //*************************************************************************
 void DrawJoint()
 {
-	// first draw the reference system
-	DrawReferenceSystem();
+    // first draw the reference system
+    DrawReferenceSystem();
 
 	// Draw the joint as a parallelepiped (a cube scaled on the y-axis)
-	// the bottom face of the cube must be on the xz plane
+    //**********************************
+    // Bring the cube "up" so that the bottom face is on the xz plane
+    //**********************************
+    glTranslatef (0.0, 1.,0 );
 
-
-
-	//**********************************
-	// draw the parallelepiped
-	//**********************************
-
-
-
-
+    //**********************************
+    // draw the scaled cube. Remember that the scaling has to be only
+    // on the local reference system, hence we need to get a local copy
+    // of the modelview matrix...
+    //**********************************
+    glPushMatrix();
+    glScalef(1,2,1);
+    glutWireCube(1);
+    glPopMatrix();
 }
 
 // Function that draws the robot as three parallelepipeds
 void DrawRobot()
 {
-	//**********************************
-	// It's better to work on a local reference system...
-	//**********************************
+    //**********************************
+    // we work on a copy of the current MODELVIEW matrix, hence we need to...
+    //**********************************
+    glPushMatrix();
 
-	// draw the first joint
-	DrawJoint();
+    // draw the first joint
+    DrawJoint();
+    glTranslatef (0.0, 1.,0 );
 
-	// Draw the other joints
-	// every joint must be placed on top of the previous one
-	// and rotated according to the relevant Angle
-	//**********************************
-	// the second joint
-	//**********************************
+    // // Draw the other joints
+    // // every joint must be placed on top of the previous one
+    // // and rotated according to the relevant Angle
+    //**********************************
+    // the second joint
+    //**********************************
+    glRotatef (Angle1, 1.0, 0.0, 0.0);
+    DrawJoint();
+    //**********************************
+    // the third joint
+    //**********************************
+    glTranslatef (0.0, 1.,0 );
+    glRotatef (Angle2, 1.0, 0.0, 0.0);
+    DrawJoint();
 
+    // // Draw the Pincers
+    // /First the base
+    glTranslatef (0.0, 1.125,0 );
+    glPushMatrix();
+    glScalef(1,0.25,0.25);
+    glutWireCube(1);
+    glPopMatrix();
+    glTranslatef (0.0, 0.45,0 );
+    // The the fork
+    glPushMatrix();
+    glTranslatef (-START + open*(START-END), 0.0,0 );
+    glScalef(0.25,0.65,0.25);
+    glutWireCube(1);
+    glPopMatrix();
+    glPushMatrix();
+    glTranslatef (START + open*(END-START), 0.0,0 );
+    glScalef(0.25,0.65,0.25);
+    glutWireCube(1);
+    glPopMatrix();
 
-
-	//**********************************
-	// the third joint
-	//**********************************
-
-
-
-	//**********************************
-	// "Release" the copy of the current MODELVIEW matrix
-	//**********************************
+    //**********************************
+    // "Release" the copy of the current MODELVIEW matrix
+    //**********************************
+    glPopMatrix();
 
 }
 
@@ -117,47 +173,59 @@ void DrawRobot()
 //*************************************************************************
 void display(void)
 {
-	// clear the window
-	glClear (GL_COLOR_BUFFER_BIT);
+    // clear the window
+    glClear (GL_COLOR_BUFFER_BIT);
 
-	// working with the GL_MODELVIEW Matrix
-	glMatrixMode(GL_MODELVIEW);
+    // working with the GL_MODELVIEW Matrix
+    glMatrixMode(GL_MODELVIEW);
 
-	//**********************************
-	// we work on a copy of the current MODELVIEW matrix, hence we need to...
-	//**********************************
+    //**********************************
+    // we work on a copy of the current MODELVIEW matrix, hence we need to...
+    //**********************************
+    glPushMatrix();
+    DrawReferenceSystem();
+    //**********************************
+    // Rotate the robot around the x-axis according to the relevant angle
+    //**********************************
+    glRotatef(RobotAngleX,1,0,0);
+    //**********************************
+    // Rotate the robot around the y-axis according to the relevant angle
+    //**********************************
+    glRotatef(RobotAngleY,0,1,0);
 
+    // draw the robot
+    DrawRobot();
 
-	//**********************************
-	// Rotate the robot around the x-axis and y-axis according to the relevant angles
-	//**********************************
+    //**********************************
+    // "Release" the copy of the current MODELVIEW matrix
+    //**********************************
+    glPopMatrix();
 
-
-
-	// draw the robot
-	DrawRobot();
-
-	//**********************************
-	// not anymore on a local reference system
-	//**********************************
-
-	// flush drawing routines to the window
-	glutSwapBuffers();
+    // flush drawing routines to the window
+    glutSwapBuffers();
 }
 
 //*************************************************************************
 // Special keys callback
 //*************************************************************************
-void arrows (int key, int x, int y)
-{
-		//**********************************
-		// Manage the update of RobotAngleX and RobotAngleY with the arrow keys
-		//**********************************
-
-
-
-
-		glutPostRedisplay ();
+void arrows (int key, int x, int y) {
+    switch (key)
+    {
+        case GLUT_KEY_UP:
+            RobotAngleX = (RobotAngleX+5) % 360;
+            break;
+        case GLUT_KEY_DOWN:
+            RobotAngleX = (RobotAngleX-5) % 360;
+            break;
+        case GLUT_KEY_LEFT:
+            RobotAngleY = (RobotAngleY+5) % 360;
+            break;
+        case GLUT_KEY_RIGHT:
+            RobotAngleY = (RobotAngleY-5) % 360;
+            break;
+        default: break;
+    }
+    glutPostRedisplay ();
 }
 
 //*************************************************************************
@@ -165,54 +233,70 @@ void arrows (int key, int x, int y)
 //*************************************************************************
 void keyboard (unsigned char key, int x, int y)
 {
-	switch (key)
-	{
-		case 'q':
-		case 27:
-			exit(0);
-		break;
-		//**********************************
-		// Manage the update of Angle1 with the key 'a' and 'z'
-		//**********************************
+    switch (key)
+    {
+        case 'q':
+        case 27:
+            exit(0);
+            break;
+            //**********************************
+            // Manage the update of Angle1 with the key 'a' and 'z'
+            //**********************************
+        case 'a':
+            Angle1 = (Angle1+5) % 360;
+//            printf("Angle 1: %f\n",(float)Angle1);
+            break;
+        case 'z':
+            Angle1 = (Angle1-5) % 360;
+//            printf("Angle 1: %f\n",(float)Angle1);
+            break;
+            //**********************************
+            // Manage the update of Angle2 with the key 'e' and 'r'
+            //**********************************
+        case 'e':
+            Angle2 = (Angle2+5) % 360;
+//            printf("Angle 2: %f\n",(float)Angle2);
+            break;
+        case 'r':
+            Angle2 = (Angle2-5) % 360;
+//            printf("Angle 2: %f\n",(float)Angle2);
+            break;
 
+            //**********************************
+            // Manage the pincers
+            //**********************************
+        case 'o':
+            open += 0.05f;
+            if (open > 1) open = 1;
 
+            break;
+        case 'l':
+            open -= 0.05f;
+            if (open < 0) open = 0;
 
+            break;
 
-
-
-
-		//**********************************
-		// Manage the update of Angle2 with the key 'e' and 'r'
-		//**********************************
-
-
-
-
-
-
-
-
-		default:
-        break;
-	}
-	glutPostRedisplay ();
+        default:
+            break;
+    }
+    glutPostRedisplay ();
 }
 
 
 
 void init(void)
 {
-	glClearColor (0.0, 0.0, 0.0, 0.0);
-	glMatrixMode (GL_PROJECTION);
-	glLoadIdentity ();
-	gluPerspective(65.0, 1.0, 1.0, 100.0);
+    glClearColor (0.0, 0.0, 0.0, 0.0);
+    glMatrixMode (GL_PROJECTION);
+    glLoadIdentity ();
+    gluPerspective(65.0, 1.0, 1.0, 100.0);
 
-	glShadeModel (GL_FLAT);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+    glShadeModel (GL_FLAT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 
-	// Place the camera
-	gluLookAt(-6,5,-6,0,0,2,0,1,0);
+    // Place the camera
+    gluLookAt(-1,5,-1,0,0,2,0,1,0);
 }
 
 
@@ -231,45 +315,51 @@ void reshape ( int width, int height )
 }
 
 
+
 //*************************************************************************
 // Prints out how to use the keyboard
 //*************************************************************************
 void usage()
 {
-	printf ("\n*******\n");
-	printf ("Arrows key: rotate the whole robot\n");
-	printf ("[a][z] : move the second joint of the arm\n");
-	printf ("[e][r] : move the third joint of the arm\n");
-	printf ("[esc]  : terminate\n");
-	printf ("*******\n");
+    printf ("\n*******\n");
+    printf ("Arrows key: rotate the whole robot\n");
+    printf ("[a][z] : move the second joint of the arm\n");
+    printf ("[e][r] : move the third joint of the arm\n");
+    printf ("[o][l] : move the pincers\n");
+    printf ("[esc]  : terminate\n");
+    printf ("*******\n");
 }
 
 //////////////////////////////////////////////////////////////
 int main(int argc, char** argv)
 {
-   glutInit(&argc, argv);
-   glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
-   glutInitWindowSize (500, 500);
-   glutInitWindowPosition (100, 100);
-   glutCreateWindow(argv[0]);
-   init ();
-   glutDisplayFunc(display);
+    glutInit(&argc, argv);
+    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize (500, 500);
+    glutInitWindowPosition (100, 100);
+    glutCreateWindow(argv[0]);
+    init ();
+    glutDisplayFunc(display);
 
-   glutReshapeFunc(reshape);
+    glutReshapeFunc(reshape);
     //**********************************
-   // Register the keyboard function
-   //**********************************
+    // Register the keyboard function
+    //**********************************
+    glutKeyboardFunc(keyboard);
 
-   //**********************************
-   // Register the special key function
-   //**********************************
+    //**********************************
+    // Register the special key function
+    //**********************************
+    glutSpecialFunc (arrows);
 
-   // just print the help
-   usage();
+    // just print the help
+    usage();
 
-   glutMainLoop();
+    glutMainLoop();
 
-   return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 //////////////////////////////////////////////////////////////
+
+
